bb49a10f2ae2c6af944a1691fcbe1e9d7b9c2a5e
Sumith Damodaran <sumithpd@gmail.com>
2021-02-18 15:40:22 +0100

    Added BodyCopy and foundation sitecore Helper

--- src/Feature/PageContent/code/Templates.cs
+++ src/Feature/PageContent/code/Templates.cs
@@ -6,7 +6,18 @@ using Sitecore.Data;
 
 namespace Sitecon.Feature.PageContent
 {
-  public struct Templates
+  public static class Templates
   {
+
+    public static class BodyCopy
+    {
+
+      public static class Fields
+      {
+
+        public static readonly ID BodyCopy = new ID("{47630014-446B-41C0-93E9-5E735B641BB2}");
+      }
+    }
   }
+  
 }
---
+++ src/Foundation/SitecoreHelperExtensions/code/SitecoreHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Data;
+using Sitecore.Mvc.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Sitecon.Foundation.SitecoreHelperExtensions
+{
+  public static class SitecoreHelperExtensions
+  {
+    public static HtmlString Field(this SitecoreHelper helper, ID fieldID)
+    {
+      return helper.Field(fieldID.ToString());
+    }
+
+    public static HtmlString Field(this SitecoreHelper helper, ID fieldID, object parameters)
+    {
+      return helper.Field(fieldID.ToString(), parameters);
+    }
+  }
+}
